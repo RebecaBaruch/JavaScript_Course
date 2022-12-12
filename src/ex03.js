7fcfe5c89770a4bcf889a8a13d53b909dcf4f012
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2022-12-12 17:21:45 -0300

    Exercises

---
+++ src/ex03.js
@@ -0,0 +1,6 @@
+console.log(isLandScape(300, 600));
+
+function isLandScape(width, height) {
+    return (witdh > height);
+    //we don't need de true or false statement because it's ugly
+}
---
+++ src/exe02.js
@@ -0,0 +1,10 @@
+//Writea function that takes two numbers and returns the maximum of the two
+let number = max (1, 2);
+console.log(number);
+
+function max(num1, num2) {
+    // if(num1 > num2) return num1;
+    // return num2; 
+
+    return (num1 > num2) ? num1 : num2;
+}
